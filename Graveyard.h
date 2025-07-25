890d836320ef8199af3e7237b0f15b4012c2b076
Yue Yuan <y335yuan@uwaterloo.ca>
2025-07-24 22:00:03 -0400

    Update Graveyard.h

---
+++ Graveyard.h
@@ -0,0 +1,31 @@
+#ifndef GRAVEYARD_H
+#define GRAVEYARD_H
+
+#include <vector>
+#include <memory>
+#include "Minion.h"
+
+class Graveyard {
+public:
+    void add(std::unique_ptr<Minion> m) {
+        minions_.push_back(std::move(m));
+    }
+
+    Minion* top() const {
+        return minions_.empty() ? nullptr : minions_.back().get();
+    }
+
+    std::unique_ptr<Minion> removeTop() {
+        if (minions_.empty()) return nullptr;
+        auto m = std::move(minions_.back());
+        minions_.pop_back();
+        return m;
+    }
+
+    bool empty() const {
+        return minions_.empty();
+    }
+
+private:
+    std::vector<std::unique_ptr<Minion>> minions_;
+};
